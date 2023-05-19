be9af41ed7247015857ee7a34775502510ecd531
ankitsinghyadav81967 <ankitsinghyadav81967@gmail.com>
2023-05-19 16:48:44 +0530

    Create Sequence_detector.sv

---
+++ Sequence_detector.sv
@@ -0,0 +1,76 @@
+// Code your design here
+module seq_detector_1010(input bit clk, rst_n, x, output z);
+  parameter A = 4'h1;
+  parameter B = 4'h2;
+  parameter C = 4'h3;
+  parameter D = 4'h4;
+  
+  bit [3:0] state, next_state;
+  always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin 
+      state <= A;
+    end
+    else state <= next_state;
+  end
+  
+  always @(state or x) begin
+    case(state)
+      A: begin
+           if(x == 0) next_state = A;
+           else       next_state = B;
+         end
+      B: begin
+           if(x == 0) next_state = C;
+           else       next_state = B;
+         end
+      C: begin
+           if(x == 0) next_state = A;
+           else       next_state = D;
+         end
+      D: begin
+        if(x == 0) next_state = A; //This state only differs when compared with Mealy Overlaping Machine
+           else       next_state = B;
+         end
+      default: next_state = A;
+    endcase
+  end
+  assign z = (state == D)? 1:0;
+endmodule
+
+
+//
+// Code your testbench here
+// or browse Examples
+module TB;
+  reg clk, rst_n, x;
+  wire z;
+  
+  seq_detector_1010 sd(clk, rst_n, x, z);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    $monitor("%0t: x = %0b, z = %0b", $time, x, z);
+    x = 0;
+    #1 rst_n = 0;
+    #2 rst_n = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+
+  end
+endmodule
