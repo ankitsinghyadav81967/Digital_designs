665990407120dfcdffa780ed0cd2af1b8eae203e
ankitsinghyadav81967 <ankitsinghyadav81967@gmail.com>
2023-05-19 16:35:26 +0530

    Update Traffic_light_control_system.v

--- Traffic_light_control_system.v
+++ Traffic_light_control_system.v
@@ -217,3 +217,30 @@ always @(state)
             endcase // case (state)
      end // always @ (state)
 endmodule
+
+
+// Code your testbench here
+// or browse Examples
+`timescale 1ns/1ps
+module traffic_control_tb;
+
+wire [2:0] n_lights,s_lights,e_lights,w_lights;
+reg clk,rst_a;
+
+traffic_control DUT (n_lights,s_lights,e_lights,w_lights,clk,rst_a);
+
+initial
+ begin
+  clk=1'b1;
+  forever #5 clk=~clk;
+ end
+ 
+initial
+ begin
+  rst_a=1'b1;
+  #15;
+  rst_a=1'b0;
+  #1000;
+  $stop;
+ end
+endmodule
